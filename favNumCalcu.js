ffae88c3a8e0321d02c2aa65637f7d71f5e2635f
Desiree Ann Guzman <desireeann000@gmail.com>
2024-11-04 18:22:01 +0800

    Commit with favNumCalcu.js

---
+++ favNumCalcu.js
@@ -0,0 +1,15 @@
+let favNumber = Math.floor(Math.random() * 15) + 1; 
+
+let guess;
+while (guess !== favNumber) {
+    guess = parseInt(prompt("Guess my favorite number from 0-15."));
+
+    if (guess < favNumber) {
+        alert("Too low, try again!");
+    } else if (guess > favNumber){
+        alert("Too high, try again!")
+    } else {
+        console.log("Correct, you guessed my favorite number!");
+        break;
+    }
+}
